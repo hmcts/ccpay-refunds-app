1780bef7fe274bf1bbcadde9a588f1406f7d3067
Pettedson John <pettedson.john@hmcts.net>
2021-07-27 19:22:50 +0100

    Fixing the Smoke Tests with Junit 5

--- src/test/java/uk/gov/hmcts/reform/refunds/controllers/RootControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/refunds/controllers/RootControllerTest.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.reform.refunds.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.context.WebApplicationContext;
