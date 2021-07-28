0b1a9a22172cf8a206bb008aebc844d3497bbad6
Pettedson John <pettedson.john@hmcts.net>
2021-07-28 15:01:36 +0100

    Added all of the checkstyled Smoke Tests.....

--- src/integrationTest/java/uk/gov/hmcts/reform/refunds/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/refunds/config/SwaggerPublisherTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.refunds.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Built-in feature which saves service's swagger specs in temporary directory.
  * Each travis run on master should automatically save and upload (if updated) documentation.
  */
+@Slf4j
 @WebMvcTest
 @ContextConfiguration(classes = SwaggerConfiguration.class)
 @AutoConfigureMockMvc
@@ -32,6 +34,7 @@ class SwaggerPublisherTest {
     @Test
     @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
     public void generateDocs() throws Exception {
+        log.info("TEST : - generateDocs() started");
         byte[] specs = mvc.perform(get("/v2/api-docs"))
             .andExpect(status().isOk())
             .andReturn()
@@ -42,5 +45,6 @@ class SwaggerPublisherTest {
             outputStream.write(specs);
         }
         assertTrue(specs.length > 0,"Sample message for the Test Template to function....");
+        log.info("TEST : - generateDocs() finished");
     }
 }
