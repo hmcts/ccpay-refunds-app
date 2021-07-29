27a0742ca87489bc9bdd9b054b0e5aac15d04e59
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-07-29 13:05:41 +0100

    Add developers group to product

--- infrastructure/refund_status.tf
+++ infrastructure/refund_status.tf
@@ -4,8 +4,8 @@ module "ccpay-refund-status-product" {
   api_mgmt_name = local.api_mgmt_name
   api_mgmt_rg   = local.api_mgmt_rg
   name          = var.product_name
-  
-  product_access_control_groups = ["administrators", "developers"]
+
+  product_access_control_groups = ["developers"]
 }
 
 module "ccpay-refund-status-api" {
