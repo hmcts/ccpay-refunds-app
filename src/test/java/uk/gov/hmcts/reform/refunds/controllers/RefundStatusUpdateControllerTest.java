9bf90c718e5357049e19aae1095b1b404392c5c7
aravindan89 <aravindan.ranjan@hmcts.net>
2021-07-30 17:25:28 +0100

    Test class added.

---
+++ src/test/java/uk/gov/hmcts/reform/refunds/controllers/RefundStatusUpdateControllerTest.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.refunds.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles({"local", "test"})
+public class RefundStatusUpdateControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void getRefundStatusUpdateBadRequest() throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(patch("/refund/{reference}","12345")
+                                                       .header("ServiceAuthorization", "Services")
+                                                       .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest())
+            .andReturn();
+    }
+}
+
+
