11492d2080a7f53340dbf297a6cee1687d77f7f7
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-08-12 12:14:58 +0100

    test cases added

--- src/test/java/uk/gov/hmcts/reform/refunds/controllers/RefundControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/refunds/controllers/RefundControllerTest.java
@@ -100,7 +100,7 @@ public class RefundControllerTest {
     @Mock
     private RefundReasonRepository refundReasonRepository;
 
-    @MockBean
+    @Mock
     @Qualifier("restTemplateIdam")
     private RestTemplate restTemplateIdam;
 
