1bea6e76a95d41f42a0d075b7c9125254eacf787
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-08-13 13:42:15 +0100

    Update RefundsServiceImpl.java

--- src/main/java/uk/gov/hmcts/reform/refunds/services/RefundsServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/services/RefundsServiceImpl.java
@@ -18,7 +18,6 @@ import uk.gov.hmcts.reform.refunds.repository.RefundsRepository;
 import uk.gov.hmcts.reform.refunds.repository.RejectionReasonRepository;
 import uk.gov.hmcts.reform.refunds.utils.ReferenceUtil;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -34,6 +33,8 @@ public class RefundsServiceImpl implements RefundsService {
     private static final Logger LOG = LoggerFactory.getLogger(RefundsServiceImpl.class);
 
     private static final Pattern REASONPATTERN = Pattern.compile("(^RR004-[a-zA-Z]+)|(RR004$)");
+
+    private static final Pattern STATUSPATTERN = Pattern.compile("[^rejected]");
 
     private static int reasonPrefixLength = 6;
 
@@ -97,7 +98,7 @@ public class RefundsServiceImpl implements RefundsService {
     @Override
     public List<String> getRejectedReasons() {
         // Getting names from Rejection Reasons List object
-        return rejectionReasonRepository.findAll().stream().map(r->r.getName())
+        return rejectionReasonRepository.findAll().stream().map(r -> r.getName())
             .collect(Collectors.toList());
     }
 
@@ -108,8 +109,8 @@ public class RefundsServiceImpl implements RefundsService {
 //        }
 
         Optional<List<Refund>> refundsList = refundsRepository.findByPaymentReference(refundRequest.getPaymentReference());
-        long refundProcessingCount = refundsList.get().stream().map(refund -> !refund.getRefundStatus().equals(
-            "rejected")).count();
+        long refundProcessingCount = refundsList.isPresent() ? refundsList.get().stream().map(refund -> STATUSPATTERN.matcher(
+            refund.getRefundStatus().getName()).find()).count() : 0;
 
         if (refundProcessingCount > 0) {
             throw new InvalidRefundRequestException("Refund is already processed/ in progress");
@@ -137,6 +138,7 @@ public class RefundsServiceImpl implements RefundsService {
     private Refund initiateRefundEntity(RefundRequest refundRequest, String uid) throws CheckDigitException {
         return Refund.refundsWith()
             .amount(refundRequest.getRefundAmount())
+            .ccdCaseNumber(refundRequest.getCcdCaseNumber())
             .paymentReference(refundRequest.getPaymentReference())
             .reason(refundRequest.getRefundReason())
             .refundStatus(SUBMITTED)
