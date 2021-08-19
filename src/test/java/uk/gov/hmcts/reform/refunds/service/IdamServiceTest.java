69d4468551799374ba78d5aca39a13351665e59e
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-08-19 12:42:36 +0100

    Update IdamServiceTest.java

--- src/test/java/uk/gov/hmcts/reform/refunds/service/IdamServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/refunds/service/IdamServiceTest.java
@@ -6,6 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
