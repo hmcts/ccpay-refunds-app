834a225ebab4660810909969bda6e39cc345bccc
Pettedson John <pettedson.john@hmcts.net>
2021-08-22 20:22:03 +0100

    Executing Smoke tests using the Junit 5 libraries....

--- src/smokeTest/java/uk/gov/hmcts/reform/refunds/smoke/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/refunds/smoke/SmokeTest.java
@@ -2,32 +2,36 @@ package uk.gov.hmcts.reform.refunds.smoke;
 
 import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.refunds.RefundApplication;
 
 import static io.restassured.RestAssured.given;
-import static junit.framework.TestCase.assertFalse;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 
-
-@RunWith(SpringRunner.class)
 @Slf4j
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("smoke")
+@SpringBootTest(classes = {RefundApplication.class})
 public class SmokeTest {
-    @Value("${TEST_URL:http://localhost:8080}")
+    @Value("${test.url}")
     private String testUrl;
 
-    @Before
-    public void setUp() {
+    @BeforeAll
+    void setUp() {
         RestAssured.baseURI = testUrl;
     }
 
     @Test
-    public void healthCheck() {
+    void healthCheck() {
+        log.info("TEST - healthCheck() started");
         given()
             .relaxedHTTPSValidation()
             .header(CONTENT_TYPE, "application/json")
@@ -37,6 +41,7 @@ public class SmokeTest {
             .statusCode(200)
             .body(
                 "status", equalTo("UP"));
-        assertFalse("passed",testUrl.isEmpty());
+        assertFalse(testUrl.isEmpty(), "Sample Test for the template....");
+        log.info("TEST - healthCheck() finished");
     }
 }
