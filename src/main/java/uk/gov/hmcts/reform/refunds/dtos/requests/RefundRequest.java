82e03a4f1c0e4fa75f3d82516f651d3bb46a5eb4
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-08-25 16:10:27 +0100

    Update RefundRequest.java

--- src/main/java/uk/gov/hmcts/reform/refunds/dtos/requests/RefundRequest.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/dtos/requests/RefundRequest.java
@@ -51,7 +51,7 @@ public class RefundRequest {
 
     @NotNull(message = "fee_ids cannot be null")
     @NotEmpty(message = "fee_ids cannot be blank")
-    @Pattern(regexp = "^(\\d)(,\\d)*$", message = "fee_ids is not in valid format")
+    @Pattern(regexp = "^(0*[1-9]\\d*(?:[\\., ]\\d+)*)$", message = "fee_ids is not in valid format")
     private String feeIds;
 
 }
