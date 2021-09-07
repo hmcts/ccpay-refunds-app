856b4233fe7b62f482d3686cf97e1a2184370461
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-09-07 22:50:02 +0100

    ServiceAuthTokenConfiguration addded

--- src/main/java/uk/gov/hmcts/reform/refunds/config/ServiceAuthTokenConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/config/ServiceAuthTokenConfiguration.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.refunds.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +9,6 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
 public class ServiceAuthTokenConfiguration {
-    private static final Logger LOG = LoggerFactory.getLogger(ServiceAuthTokenConfiguration.class);
     @Bean
     public AuthTokenGenerator authTokenGenerator(
         @Value("${idam.s2s-auth.totp_secret}") final String secret,
