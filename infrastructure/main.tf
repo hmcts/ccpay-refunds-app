18a0eda6009729f4378bddedbe3be069fb125ea7
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-09-17 14:07:08 +0100

    changes for bringing the  s2s secret to refunds key vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,6 +5,9 @@ provider "azurerm" {
 locals {
   vaultName = join("-", [var.core_product, var.env])
   s2sUrl = "http://rpe-service-auth-provider-${var.env}.service.core-compute-${var.env}.internal"
+  s2s_rg_prefix               = "rpe-service-auth-provider"
+  s2s_key_vault_name          = var.env == "preview" || var.env == "spreview" ? join("-", ["s2s", "aat"]) : join("-", ["s2s", var.env])
+  s2s_vault_resource_group    = var.env == "preview" || var.env == "spreview" ? join("-", [local.s2s_rg_prefix, "aat"]) : join("-", [local.s2s_rg_prefix, var.env])
 }
 
 data "azurerm_key_vault" "refunds_key_vault" {
@@ -62,5 +65,18 @@ resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   key_vault_id = data.azurerm_key_vault.refunds_key_vault.id
 }
 
+data "azurerm_key_vault" "s2s_key_vault" {
+  name                = local.s2s_key_vault_name
+  resource_group_name = local.s2s_vault_resource_group
+}
 
+data "azurerm_key_vault_secret" "s2s_secret" {
+  name          = "microservicekey-refunds-api"
+  key_vault_id  = data.azurerm_key_vault.s2s_key_vault.id
+}
 
+resource "azurerm_key_vault_secret" "refunds_s2s_secret" {
+  name          = "refunds-s2s-secret"
+  value         = data.azurerm_key_vault_secret.s2s_secret.value
+  key_vault_id  = data.azurerm_key_vault.refunds_key_vault.id
+}
