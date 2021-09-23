cab9f39984a354b9a4cee3f7277927d74de95c91
vigneshpushkaranHMCTS <vignesh.pushkaran@hmcts.net>
2021-09-23 14:43:57 +0100

    Update RefundApplication.java

--- src/main/java/uk/gov/hmcts/reform/refunds/RefundApplication.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/RefundApplication.java
@@ -3,7 +3,11 @@ package uk.gov.hmcts.reform.refunds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCaching
+@EnableFeignClients
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class RefundApplication {
