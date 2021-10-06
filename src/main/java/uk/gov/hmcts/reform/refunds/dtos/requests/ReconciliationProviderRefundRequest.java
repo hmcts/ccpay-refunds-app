41e81fb153d7bcea947798650906aaf968b9b299
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-10-06 14:20:41 +0100

    altered the liberata refund Reques

---
+++ src/main/java/uk/gov/hmcts/reform/refunds/dtos/requests/ReconciliationProviderRefundRequest.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.refunds.dtos.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(builderMethodName = "refundReconciliationProviderRefundRequestWith")
+public class ReconciliationProviderRefundRequest {
+    private ReconciliationProviderRequest refundRequest;
+}
