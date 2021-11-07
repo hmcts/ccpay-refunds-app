0bc4ecacb8a6fd5444595e27a6deaec62ea03c5e
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2021-11-07 19:12:18 +0000

    updated

--- src/main/java/uk/gov/hmcts/reform/refunds/services/RefundsServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/services/RefundsServiceImpl.java
@@ -415,7 +415,6 @@ public class RefundsServiceImpl extends StateUtil implements RefundsService {
                 .filter(refundReason -> refundReason.getCode().equalsIgnoreCase(rawReason))
                 .collect(Collectors.toList());
             if (!refundReasonOptional.isEmpty()) {
-                System.out.println();
                 return refundReasonOptional.get(0).getName();
             }
             throw new RefundReasonNotFoundException(rawReason);
