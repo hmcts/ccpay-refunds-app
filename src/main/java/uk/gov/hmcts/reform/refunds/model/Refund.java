f350309c1ce8e041b0adfa14a5515ddd48d3c7e1
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2021-12-02 14:53:58 +0000

    updated

--- src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
@@ -1,31 +1,13 @@
 package uk.gov.hmcts.reform.refunds.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
-
 import java.sql.Timestamp;
-
 import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
 @Builder(builderMethodName = "refundsWith")
