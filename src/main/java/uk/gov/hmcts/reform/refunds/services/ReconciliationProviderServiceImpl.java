9a601f152c4aa10d71e7bf1849da2ae51debb4ab
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-12-09 11:46:01 +0000

    ReconciliationProviderServiceImpl

--- src/main/java/uk/gov/hmcts/reform/refunds/services/ReconciliationProviderServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/services/ReconciliationProviderServiceImpl.java
@@ -40,8 +40,8 @@ public class ReconciliationProviderServiceImpl implements ReconciliationProvider
             .refundReconciliationProviderRefundRequestWith()
             .refundRequest(reconciliationProviderRequest).build();
         try {
-
             headers.add("X-API-KEY", apiKey);
+
             UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(reconciliationProviderApi + refundStatusUpdatePath);
             return restTemplate.exchange(
                 builder.toUriString(),
