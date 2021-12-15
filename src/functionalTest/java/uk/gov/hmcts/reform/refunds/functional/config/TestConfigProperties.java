cd7bcc87a57392762f085214751d0990a47459d0
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2021-12-15 14:25:16 +0000

    rebased with refunds_release_check

--- src/functionalTest/java/uk/gov/hmcts/reform/refunds/functional/config/TestConfigProperties.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/refunds/functional/config/TestConfigProperties.java
@@ -59,4 +59,9 @@ public class TestConfigProperties {
     @Value("${s2s.service.cmc.secret}")
     public String cmcS2SSecret;
 
+    @Value("${idam.paybubble.client.id}")
+    public String idamPayBubbleClientId;
+
+    @Value("${idam.paybubble.client.secret}")
+    public String idamPayBubbleClientSecret;
 }
--- src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
@@ -11,7 +11,9 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+
 import java.sql.Timestamp;
+
 import java.util.List;
 
 import javax.persistence.CascadeType;
