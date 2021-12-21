6a6ed277b93859bcef43544b698245ded445ab91
ranjeet-kumarhmcts <ranjeet.kumar@hmcts.net>
2021-12-21 17:41:59 +0000

    added contact_details model

--- src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.refunds.model;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import com.vladmihalcea.hibernate.type.json.JsonType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
