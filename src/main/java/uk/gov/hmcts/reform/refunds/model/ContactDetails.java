ade234877d6d87884d49eafdb3237a64c81121a1
“sayali-salunkhe” <sayali.salunkhe@HMCTS.net>
2022-01-09 21:29:50 +0000

    PAY-5360 updated import statements

--- src/main/java/uk/gov/hmcts/reform/refunds/model/ContactDetails.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/model/ContactDetails.java
@@ -2,7 +2,10 @@ package uk.gov.hmcts.reform.refunds.model;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
