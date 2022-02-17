50299a87b77c0bbc68e2bdbd8bf9c3cedd8948a4
“sayali-salunkhe” <sayali.salunkhe@HMCTS.net>
2022-02-17 13:15:53 +0000

    fixed checkstyle issues

--- src/functionalTest/java/uk/gov/hmcts/reform/refunds/functional/request/PaymentRefundRequest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/refunds/functional/request/PaymentRefundRequest.java
@@ -9,11 +9,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotEmpty;
-import java.math.BigDecimal;
-import java.util.List;
 import javax.validation.constraints.NotNull;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
