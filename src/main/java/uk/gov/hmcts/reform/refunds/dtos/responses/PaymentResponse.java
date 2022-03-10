b9a3081b51237351c34d9076585daaf61f2db712
Anooj Kurup <anooj.kurup@hmcts.net>
2022-03-10 00:55:18 +0000

    Fixed roles in FT so that payment will be returned from payment app. Removed logs which were added for debugging.

--- src/main/java/uk/gov/hmcts/reform/refunds/dtos/responses/PaymentResponse.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/dtos/responses/PaymentResponse.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.refunds.dtos.responses;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,11 +12,8 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
-
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-@JsonInclude(NON_NULL)
+@JsonNaming(SnakeCaseStrategy.class)
 @Builder(builderMethodName = "paymentResponseWith")
 @AllArgsConstructor
 @Getter
