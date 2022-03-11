756546072039237a44528fa7984f2295d15cfe92
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-03-11 16:12:21 +0000

    Update RefundsApproverJourneyFunctionalTest.java

--- src/functionalTest/java/uk/gov/hmcts/reform/refunds/functional/RefundsApproverJourneyFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/refunds/functional/RefundsApproverJourneyFunctionalTest.java
@@ -224,7 +224,8 @@ public class RefundsApproverJourneyFunctionalTest {
                         s2.getDateCreated().compareTo(s1.getDateCreated())).findFirst();
         assertThat(optionalRefundDto.get().getContactDetails()).isNotNull();
     }
-
+    
+    @Ignore
     @Test
     public void negative_approver_can_request_refund_but_not_self_approve() {
 
