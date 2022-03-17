f951a7d2d8411bb12eccaa16bb7e211868733633
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-03-17 16:13:49 +0000

    Update refund_list.tf

--- infrastructure/refund_list.tf
+++ infrastructure/refund_list.tf
@@ -1,27 +1,27 @@
-module "ccpay-refund-list-product" {
+module "ccpay-refund-lists-product" {
   source = "git@github.com:hmcts/cnp-module-api-mgmt-product?ref=master"
 
-  api_mgmt_name = local.api_mgmt_name
-  api_mgmt_rg   = local.api_mgmt_rg
+  api_mgmt_name = local.api_mgmt_name_cft
+  api_mgmt_rg   = local.api_mgmt_rg_cft
   name = var.product_name
   product_access_control_groups = ["developers"]
 }
 
-module "ccpay-refund-list-api" {
+module "ccpay-refund-lists-api" {
   source = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
 
-  api_mgmt_name = local.api_mgmt_name
-  api_mgmt_rg   = local.api_mgmt_rg
+  api_mgmt_name = local.api_mgmt_name_cft
+  api_mgmt_rg   = local.api_mgmt_rg_cft
   revision      = "1"
   service_url   = local.refunds_api_url
-  product_id    = module.ccpay-refund-list-product.product_id
+  product_id    = module.ccpay-refund-lists-product.product_id
   name          = join("-", [var.product_name, "api"])
   display_name  = "Refund List API"
   path          = "refunds-api"
-  swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/ccpay-payment-app.refunds-list.json"
+  swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/ccpay-payment-app.refunds-status.json"
 }
 
-data "template_file" "refund_list_policy_template" {
+data "template_file" "refund_lists_policy_template" {
   template = file(join("", [path.module, "/template/api-policy.xml"]))
 
   vars = {
@@ -32,13 +32,54 @@ data "template_file" "refund_list_policy_template" {
   }
 }
 
-module "ccpay-refund-list-policy" {
+module "ccpay-refund-lists-policy" {
   source = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
 
-  api_mgmt_name = local.api_mgmt_name
-  api_mgmt_rg   = local.api_mgmt_rg
+  api_mgmt_name = local.api_mgmt_name_cft
+  api_mgmt_rg   = local.api_mgmt_rg_cft
 
-  api_name               = module.ccpay-refund-list-api.name
+  api_name               = module.ccpay-refund-lists-api.name
   api_policy_xml_content = data.template_file.refund_status_policy_template.rendered
 }
+
+
+
+data "azurerm_api_management_user" "refund_lists" {
+  api_management_name = local.api_mgmt_name_cft
+  resource_group_name   = local.api_mgmt_rg_cft
+  user_id             = "5931a75ae4bbd512288c680br"
+}
+
+data "azurerm_api_management" "refund_lists" {
+  name                = var.product_name
+  resource_group_name = local.api_mgmt_rg_cft
+}
+
+data "azurerm_api_management_product" "refund_lists" {
+  product_id          = module.ccpay-refund-lists-product.product_id
+  api_management_name = data.azurerm_api_management.refund_lists.name
+  resource_group_name = data.azurerm_api_management.refund_lists.resource_group_name
+}
+
+
+
+resource "azurerm_api_management_subscription" "refund_lists_subscription" {
+  api_management_name = local.api_mgmt_name_cft
+  resource_group_name   = local.api_mgmt_rg_cft
+  user_id             = data.azurerm_api_management_user.refund_lists.id
+  product_id          = data.azurerm_api_management_product.refund_lists.id
+  display_name        = "Refund List Subscription"
+  state               = "active"
+}
+
+resource "azurerm_api_management_user" "refund_lists" {
+  user_id             = "5931a75ae4bbd512288c680br"
+  api_management_name = local.api_mgmt_name_cft
+  resource_group_name   = local.api_mgmt_rg_cft
+  first_name          = "Anshika"
+  last_name           = "Nigam"
+  email               = "anshika.nigam@hmcts.net"
+  state               = "active"
+}
+
 
