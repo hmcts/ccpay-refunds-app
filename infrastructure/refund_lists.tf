bda40a39e809f1dd710ab77796cfe09f358d59e8
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-03-17 02:04:03 +0000

    updated tf

--- infrastructure/refund_lists.tf
+++ infrastructure/refund_lists.tf
@@ -2,7 +2,7 @@ module "ccpay-refund-lists-product" {
   source = "git@github.com:hmcts/cnp-module-api-mgmt-product?ref=master"
 
   api_mgmt_name = local.api_mgmt_name_cft
-  api_mgmt_rg   = local.api_mgmt_rg_cft
+  api_mgmt_rg   = "cft-demo-network-rg"
   name = var.product_name
   product_access_control_groups = ["developers"]
 }
@@ -11,14 +11,14 @@ module "ccpay-refund-lists-api" {
   source = "git@github.com:hmcts/cnp-module-api-mgmt-api?ref=master"
 
   api_mgmt_name = local.api_mgmt_name_cft
-  api_mgmt_rg   = local.api_mgmt_rg_cft
+  api_mgmt_rg   = "cft-demo-network-rg"
   revision      = "1"
   service_url   = local.refunds_api_url
   product_id    = module.ccpay-refund-lists-product.product_id
   name          = join("-", [var.product_name, "api"])
   display_name  = "Refund List API"
   path          = "refunds-api"
-  swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/ccpay-payment-app.refunds-status.json"
+  swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/ccpay-payment-app.refunds-list.json"
 }
 
 data "template_file" "refund_lists_policy_template" {
