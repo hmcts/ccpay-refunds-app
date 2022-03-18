5f0f64199017f608a52ba8f48f01708595200d8b
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-03-18 11:10:36 +0000

    Update refund_lists.tf

--- infrastructure/refund_lists.tf
+++ infrastructure/refund_lists.tf
@@ -21,23 +21,6 @@ module "ccpay-refund-lists-api" {
   swagger_url   = "https://raw.githubusercontent.com/hmcts/reform-api-docs/master/docs/specs/ccpay-payment-app.refunds-list.json"
 }
 
-data "template_file" "refund_lists_policy_template" {
-  template = file(join("", [path.module, "/template/api-policy.xml"]))
-
-  vars = {
-    s2s_base_url                    = local.s2sUrl
-  }
-}
-
-module "ccpay-refund-lists-policy" {
-  source = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
-
-  api_mgmt_name = local.api_mgmt_name_cft
-  api_mgmt_rg   = local.api_mgmt_rg_cft
-
-  api_name               = module.ccpay-refund-lists-api.name
-  api_policy_xml_content = data.template_file.refund_status_policy_template.rendered
-}
 
 
 
