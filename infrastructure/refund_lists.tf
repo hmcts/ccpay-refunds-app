046c06e7cb7cf4c68562ebe5c7110a30ae8b5c26
vijayrajagopalan-hmcts <vijay.rajagopalan@hmcts.net>
2022-03-18 16:21:29 +0000

    Test providers

--- infrastructure/refund_lists.tf
+++ infrastructure/refund_lists.tf
@@ -28,49 +28,45 @@ module "ccpay-refund-lists-api" {
   }
 }
   
-data "template_file" "refund_lists_policy_template" {
-   template = file(join("", [path.module, "/template/api-policy.xml"]))
+# data "template_file" "refund_lists_policy_template" {
+#    template = file(join("", [path.module, "/template/api-policy.xml"]))
 
-   vars = {
-     allowed_certificate_thumbprints = local.refund_status_thumbprints_in_quotes_str
-     s2s_client_id                   = data.azurerm_key_vault_secret.s2s_client_id.value
-     s2s_client_secret               = data.azurerm_key_vault_secret.s2s_client_secret.value
-     s2s_base_url                    = local.s2sUrl
-   }
- }
+#    vars = {
+#      allowed_certificate_thumbprints = local.refund_status_thumbprints_in_quotes_str
+#      s2s_client_id                   = data.azurerm_key_vault_secret.s2s_client_id.value
+#      s2s_client_secret               = data.azurerm_key_vault_secret.s2s_client_secret.value
+#      s2s_base_url                    = local.s2sUrl
+#    }
+#  }
 
- module "ccpay-refund-lists-policy" {
-   source = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
+#  module "ccpay-refund-lists-policy" {
+#    source = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
 
-   api_mgmt_name = local.api_mgmt_name_cft
-   api_mgmt_rg   = local.api_mgmt_rg_cft
+#    api_mgmt_name = local.api_mgmt_name_cft
+#    api_mgmt_rg   = local.api_mgmt_rg_cft
 
-   api_name               = module.ccpay-refund-lists-api.name
-   api_policy_xml_content = data.template_file.refund_status_policy_template.rendered
+#    api_name               = module.ccpay-refund-lists-api.name
+#    api_policy_xml_content = data.template_file.refund_status_policy_template.rendered
    
- }
+#  }
 
 
 
- resource "azurerm_api_management_user" "refudList_user" {
-  api_management_name = local.api_mgmt_name_cft
-  resource_group_name   = local.api_mgmt_rg_cft
-  user_id             = "5931a75ae4bbd512288c680b"
-  first_name          = "Anshika"
-  last_name           = "Nigam"
-  email               = "anshika.nigam@hmcts.net"
-  state               = "active"
-}
+#  resource "azurerm_api_management_user" "refudList_user" {
+#   api_management_name = local.api_mgmt_name_cft
+#   resource_group_name   = local.api_mgmt_rg_cft
+#   user_id             = "5931a75ae4bbd512288c680b"
+#   first_name          = "Anshika"
+#   last_name           = "Nigam"
+#   email               = "anshika.nigam@hmcts.net"
+#   state               = "active"
+# }
 
-resource "azurerm_api_management_subscription" "refudList_subscription" {
-  api_management_name = local.api_mgmt_name_cft
-  resource_group_name   = local.api_mgmt_rg_cft
-  user_id             = azurerm_api_management_user.refudList_user.id
-  product_id          = module.ccpay-refund-lists-product.product_id
-  display_name        = "RefudList Subscription"
-  state               = "active"
-}
-
-
-
-  
+# resource "azurerm_api_management_subscription" "refudList_subscription" {
+#   api_management_name = local.api_mgmt_name_cft
+#   resource_group_name   = local.api_mgmt_rg_cft
+#   user_id             = azurerm_api_management_user.refudList_user.id
+#   product_id          = module.ccpay-refund-lists-product.product_id
+#   display_name        = "RefudList Subscription"
+#   state               = "active"
+# }
