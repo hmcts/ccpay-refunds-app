3de54289ddcf4c16e16b3f2a69638834927ccf02
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2022-03-18 15:01:28 +0000

    Update refund_lists.tf

--- infrastructure/refund_lists.tf
+++ infrastructure/refund_lists.tf
@@ -27,3 +27,55 @@ module "ccpay-refund-lists-api" {
     azurerm = azurerm.cftappsdemo
   }
 }
+  
+data "template_file" "refund_lists_policy_template" {
+   template = file(join("", [path.module, "/template/api-policy.xml"]))
+
+   vars = {
+     allowed_certificate_thumbprints = local.refund_status_thumbprints_in_quotes_str
+     s2s_client_id                   = data.azurerm_key_vault_secret.s2s_client_id.value
+     s2s_client_secret               = data.azurerm_key_vault_secret.s2s_client_secret.value
+     s2s_base_url                    = local.s2sUrl
+   }
+ }
+
+ module "ccpay-refund-lists-policy" {
+   source = "git@github.com:hmcts/cnp-module-api-mgmt-api-policy?ref=master"
+
+   api_mgmt_name = local.api_mgmt_name_cft
+   api_mgmt_rg   = local.api_mgmt_rg_cft
+
+   api_name               = module.ccpay-refund-lists-api.name
+   api_policy_xml_content = data.template_file.refund_status_policy_template.rendered
+   
+ }
+
+
+
+ data "azurerm_api_management_user" "refund_lists" {
+   api_management_name = local.api_mgmt_name_cft
+   resource_group_name   = local.api_mgmt_rg_cft
+   user_id             = "5931a75ae4bbd512288c680br"
+ }
+
+ data "azurerm_api_management" "refund_lists" {
+   name                = var.product_name
+   resource_group_name = local.api_mgmt_rg_cft
+ }
+
+ data "azurerm_api_management_product" "refund_lists" {
+   product_id          = module.ccpay-refund-lists-product.product_id
+   api_management_name = data.azurerm_api_management.refund_lists.name
+   resource_group_name = data.azurerm_api_management.refund_lists.resource_group_name
+ }
+
+
+
+ resource "azurerm_api_management_subscription" "refund_lists_subscription" {
+   api_management_name = local.api_mgmt_name_cft
+   resource_group_name   = local.api_mgmt_rg_cft
+   user_id             = data.azurerm_api_management_user.refund_lists.id
+   product_id          = data.azurerm_api_management_product.refund_lists.id
+   display_name        = "Refund List Subscription"
+   state               = "active"
+ }
