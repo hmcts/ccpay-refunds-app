4ef08753263c8702f9cc7b84d968ce3f7e0a8ad0
vijayrajagopalan-hmcts <vijay.rajagopalan@hmcts.net>
2022-03-18 16:25:35 +0000

    Test

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -14,6 +14,10 @@ terraform {
 }
 
 provider "azurerm" {
+  features {}
+}
+
+provider "azurerm" {
    alias = "cftappsdemo"
    features {}
    subscription_id = "d025fece-ce99-4df2-b7a9-b649d3ff2060"
