77aa8d849c9a800372a54dd28f9448b7be00b46f
Arbab Ahmed <78796119+arbab-cog@users.noreply.github.com>
2022-03-25 10:54:53 +0000

    Update Refund.java

--- src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
@@ -28,8 +28,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import javax.persistence.FetchType;
+import javax.persistence.Table;
 
 @Entity
 @Builder(builderMethodName = "refundsWith")
