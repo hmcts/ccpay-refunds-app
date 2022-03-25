ec318dffe762bfe10021609ffa13083edbbd06b3
Arbab Ahmed <78796119+arbab-cog@users.noreply.github.com>
2022-03-25 12:41:58 +0000

    Update Refund.java

--- src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
@@ -22,6 +22,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -102,8 +103,8 @@ public class Refund {
 
 
     //@ToString.Exclude
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "refunds_id", referencedColumnName = "id", nullable = false)
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "refunds",cascade = CascadeType.ALL)
+    //@JoinColumn(name = "refunds_id", referencedColumnName = "id", nullable = false)
     private List<RefundFees> refundFees;
 
 }
