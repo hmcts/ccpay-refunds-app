349282d9f88a949340405ec4f1d408c6adbbfa16
Arbab Ahmed <78796119+arbab-cog@users.noreply.github.com>
2022-03-25 12:08:04 +0000

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/model/Refund.java
@@ -28,7 +28,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.FetchType;
 import javax.persistence.Table;
 
 @Entity
