dcbc4da9b2ecd8dc121ce64ab07df0bd762f7027
Arbab Ahmed <78796119+arbab-cog@users.noreply.github.com>
2022-03-25 17:26:18 +0000

    Test

--- src/main/java/uk/gov/hmcts/reform/refunds/mapper/RefundResponseMapper.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/mapper/RefundResponseMapper.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.refunds.dtos.responses.RefundDto;
 import uk.gov.hmcts.reform.refunds.dtos.responses.UserIdentityDataDto;
 import uk.gov.hmcts.reform.refunds.model.Refund;
+import uk.gov.hmcts.reform.refunds.model.RefundFees;
+import uk.gov.hmcts.reform.refunds.utils.RefundFeeListMapper;
+
+import java.util.List;
 
 @Component
 public class RefundResponseMapper {
 
 
     public RefundDto getRefundListDto(Refund refund, UserIdentityDataDto userData,String reason) {
+
+        RefundFeeListMapper refundFeeListMapper = new RefundFeeListMapper();
+        List<RefundFees> refundFeesList = refundFeeListMapper.toRefundFeesList(refund);
         return RefundDto
             .buildRefundListDtoWith()
             .ccdCaseNumber(refund.getCcdCaseNumber())
@@ -25,7 +32,7 @@ public class RefundResponseMapper {
             .contactDetails(refund.getContactDetails())
             .serviceType(refund.getServiceType())
             .feeIds(refund.getFeeIds())
-            .refundFees(refund.getRefundFees())
+            .refundFees(refundFeesList)
             .build();
 
     }
