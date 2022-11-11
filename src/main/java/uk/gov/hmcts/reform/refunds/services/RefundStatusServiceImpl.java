e432d376f1d48ab8845a741cc9aa1021833dd9f6
“PragneshPatel2” <pragnesh.patel2@hmcts.net>
2022-11-11 14:06:43 +0000

    PAY-5766 Add debug line for FT fail

--- src/main/java/uk/gov/hmcts/reform/refunds/services/RefundStatusServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/services/RefundStatusServiceImpl.java
@@ -43,14 +43,18 @@ public class RefundStatusServiceImpl extends StateUtil implements RefundStatusSe
     @Override
     public ResponseEntity updateRefundStatus(String reference, RefundStatusUpdateRequest statusUpdateRequest, MultiValueMap<String, String> headers) {
         LOG.info("statusUpdateRequest: {}", statusUpdateRequest);
-        LOG.info("Reference -----> "+reference);
+        LOG.info("Reference -----> " + reference);
+
         Refund refund = refundsRepository.findByReferenceOrThrow(reference);
-        RefundState currentRefundState = getRefundState(refund.getRefundStatus().getName());
+
         LOG.info("Send Notification1 ---> " + refund.toString());
         LOG.info("Send Notification2 ---> " + refund.getContactDetails());
         LOG.info("Send Notification3 ---> " + refund.getContactDetails().getNotificationType());
         LOG.info("Send Notification4 ---> " + refund.getRefundInstructionType());
         LOG.info("Send Notification5 ---> " + refund.getReason());
+        LOG.info("Send Notification6 ---> " + refund.getRefundStatus().getName());
+
+        RefundState currentRefundState = getRefundState(refund.getRefundStatus().getName());
         if (currentRefundState.getRefundStatus().equals(RefundStatus.APPROVED)) {
             if (statusUpdateRequest.getStatus().getCode().equals(ACCEPTED)) {
                 refund.setRefundStatus(RefundStatus.ACCEPTED);
