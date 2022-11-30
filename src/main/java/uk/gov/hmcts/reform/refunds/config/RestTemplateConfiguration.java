03b37b3a432c8bb8b1267e24fb78a731beb29555
Anooj Kurup <anooj.kurup@hmcts.net>
2022-11-30 09:41:33 +0000

    use digicert during Liberata call.

--- src/main/java/uk/gov/hmcts/reform/refunds/config/RestTemplateConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/config/RestTemplateConfiguration.java
@@ -1,11 +1,22 @@
 package uk.gov.hmcts.reform.refunds.config;
 
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import javax.net.ssl.SSLContext;
 
 @Configuration
 public class RestTemplateConfiguration {
@@ -28,8 +39,15 @@ public class RestTemplateConfiguration {
     }
 
     @Bean("restTemplateLiberata")
-    public RestTemplate restTemplateLiberata() {
-        return new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+    public RestTemplate restTemplateLiberata() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException,
+        CertificateException, IOException {
+        SSLContext sslContext = new SSLContextBuilder()
+            .loadTrustMaterial(new URL("file:src/main/resources/liberatadigicert.p12"),
+                               "liberatadigicert".toCharArray()).build();
+        SSLConnectionSocketFactory sslConFactory = new SSLConnectionSocketFactory(sslContext);
+        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
+            HttpClients.custom().setSSLSocketFactory(sslConFactory).build());
+        return new RestTemplate(requestFactory);
     }
 
 }
