6e498f8a844930bdc6c459c8c6d9cc8d961aa639
“PragneshPatel2” <pragnesh.patel2@hmcts.net>
2023-03-09 13:58:05 +0000

    FT fix

--- src/functionalTest/java/uk/gov/hmcts/reform/refunds/functional/RefundsApproverJourneyFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/refunds/functional/RefundsApproverJourneyFunctionalTest.java
@@ -689,8 +689,10 @@ public class RefundsApproverJourneyFunctionalTest {
 
         assertThat(refundListResponse.getStatusCode()).isEqualTo(HttpStatus.OK.value());
         RefundListDtoResponse refundListDtoResponse = refundListResponse.getBody().as(RefundListDtoResponse.class);
-        assertEquals(refundListDtoResponse.getRefundList().size(), 1);
-        assertEquals(refundListDtoResponse.getRefundList().get(0).getServiceType(), "Probate");
+        for (RefundDto refundDto : refundListDtoResponse.getRefundList()) {
+            assertEquals(refundDto.getServiceType(), "Probate");
+        }
+
         // delete payment record
         paymentTestService
             .deletePayment(USER_TOKEN_PAYMENTS_REFUND_APPROVER_AND_PAYMENTS_ROLE, SERVICE_TOKEN_PAY_BUBBLE_PAYMENT,
