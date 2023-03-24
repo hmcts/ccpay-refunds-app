6df4885a0f418332e775acf36633a31d067c5db2
anshikanigam15 <92734361+anshikanigam15@users.noreply.github.com>
2023-03-24 11:13:32 +0000

    Update RefundApplication.java

--- src/main/java/uk/gov/hmcts/reform/refunds/RefundApplication.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/RefundApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class test
 public class RefundApplication {
 
     public static void main(final String[] args) {
-        SpringApplication.run(RefundApplication.class, args).start() ;
+        SpringApplication.run(RefundApplication.class, args).start();
     }
 
 }
