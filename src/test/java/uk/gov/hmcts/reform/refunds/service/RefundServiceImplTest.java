3981d4c279f45e528bc9ffcdb04f1c4d0f4b9805
David Jones <dave.jones@hmcts.net>
2023-07-14 17:31:32 +0100

    Correcting tests to hand servicetype for when searching refunds list with case number.

--- src/test/java/uk/gov/hmcts/reform/refunds/service/RefundServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/refunds/service/RefundServiceImplTest.java
@@ -86,7 +86,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
 
