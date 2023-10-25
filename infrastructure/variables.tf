2965ab14f723805301323736d3b0230909cd7dd0
Ollie-Smith <oliver.smith@hmcts.net>
2023-10-25 14:18:18 +0100

    PAY-6571-Flexible-Server-Setup

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -71,6 +71,8 @@ variable flexible_sku_name {
   default = "GP_Standard_D2s_v3"
 }
 
+variable "aks_subscription_id" {}
+
 variable "jenkins_AAD_objectId" {
   type                        = string
   description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
