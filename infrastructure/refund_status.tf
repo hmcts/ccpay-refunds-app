c9cb3378a7fce7a4dfa07be3fc3393383f40a5f0
58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
2024-07-10 16:52:16 +0000

    Updating Terraform Formatting

--- infrastructure/refund_status.tf
+++ infrastructure/refund_status.tf
@@ -1,9 +1,9 @@
 module "ccpay-refund-status-product" {
   source = "git@github.com:hmcts/cnp-module-api-mgmt-product?ref=master"
 
-  api_mgmt_name = local.api_mgmt_name
-  api_mgmt_rg   = local.api_mgmt_rg
-  name = var.product_name
+  api_mgmt_name                 = local.api_mgmt_name
+  api_mgmt_rg                   = local.api_mgmt_rg
+  name                          = var.product_name
   product_access_control_groups = ["developers"]
 }
 
