4bda117a029e38fc7e28cb67634eeb106cc1afb1
David Jones <dave.jones@hmcts.net>
2024-07-10 17:24:40 +0100

    PAY-6602: Refund Status API for new APIM.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,6 +11,7 @@ locals {
   # Api Management config
   api_mgmt_name     = join("-", ["core-api-mgmt", var.env])
   api_mgmt_rg       = join("-", ["core-infra", var.env])
+  api_base_path     = "refunds-api"
 
   vaultName = join("-", [var.core_product, var.env])
   s2sUrl = "http://rpe-service-auth-provider-${var.env}.service.core-compute-${var.env}.internal"
--- infrastructure/prod.tfvars
+++ infrastructure/prod.tfvars
@@ -1,4 +1,5 @@
 sku_name = "GP_Gen5_4"
 flexible_sku_name = "GP_Standard_D4s_v3"
 sku_capacity = "4"
+aks_subscription_id = "8cbc6f36-7c56-4963-9d36-739db5d00b27"
 refunds_api_gateway_certificate_thumbprints = ["B9D9E70AC23EAF8EA094F6B59EF77FF77D977CBE"]
