b09624a243e7e9af677cf7db9218c6d915146f46
David Jones <dave.jones@hmcts.net>
2024-07-15 14:09:39 +0100

    PAY-6602: Refund subscription update.

--- infrastructure/cft-api-mgmt-subscriptions.tf
+++ infrastructure/cft-api-mgmt-subscriptions.tf
@@ -17,19 +17,20 @@ resource "azurerm_key_vault_secret" "fee_pay_team_refund_subscription_key" {
 }
 
 # Supplier subscription - Liberata
-resource "azurerm_api_management_subscription" "liberata_supplier_subscription" {
+resource "azurerm_api_management_subscription" "liberata_supplier_payment_subscription" {
   api_management_name = local.cft_api_mgmt_name
   resource_group_name = local.cft_api_mgmt_rg
   product_id          = module.cft_api_mgmt_product.id
   display_name        = "Refund Status API - Liberata Subscription"
   state               = "active"
   provider            = azurerm.aks-cftapps
-  primary_key         = data.azurerm_key_vault_secret.liberata_supplier_subscription_key.value
+  primary_key         = data.azurerm_key_vault_secret.liberata_supplier_refund_subscription_key.value
 
   depends_on = [data.azurerm_key_vault_secret.liberata_supplier_refund_subscription_key]
 }
 
 data "azurerm_key_vault_secret" "liberata_supplier_refund_subscription_key" {
   name         = "liberata-cft-apim-refund-subscription-key"
+  value        = azurerm_api_management_subscription.liberata_supplier_payment_subscription.primary_key
   key_vault_id = data.azurerm_key_vault.refunds_key_vault.id
 }
