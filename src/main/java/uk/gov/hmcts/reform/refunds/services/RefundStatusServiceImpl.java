cdc63764fdb9df1c99ef4a4fcc4f6dceb2f54424
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2025-07-09 13:34:16 +0100

    PAY-7934 remove RefundWhenContacted

--- src/main/java/uk/gov/hmcts/reform/refunds/services/RefundStatusServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/services/RefundStatusServiceImpl.java
@@ -76,44 +76,6 @@ public class RefundStatusServiceImpl extends StateUtil implements RefundStatusSe
             ));
             refund.setUpdatedBy(LIBERATA_NAME);
 
-            if (null != statusUpdateRequest.getReason()
-                && statusUpdateRequest.getReason().equalsIgnoreCase(
-                    RefundsUtil.REFUND_WHEN_CONTACTED_REJECT_REASON)) {
-
-                refund.setRefundInstructionType(RefundsUtil.REFUND_WHEN_CONTACTED);
-
-                IdamTokenResponse idamTokenResponse = idamService.getSecurityTokens();
-                String authorization =  "Bearer " + idamTokenResponse.getAccessToken();
-                headers.put("authorization", Collections.singletonList(authorization));
-
-                Notification notificationDetails = notificationService.getNotificationDetails(headers, refund.getReference());
-                if (notificationDetails == null) {
-                    LOG.error("Notification not found. Not able to send notification.");
-                } else {
-                    ContactDetails newContact = ContactDetails.contactDetailsWith()
-                        .notificationType(notificationDetails.getNotificationType())
-                        .postalCode(notificationDetails.getContactDetails().getPostalCode())
-                        .city(notificationDetails.getContactDetails().getCity())
-                        .country(notificationDetails.getContactDetails().getCountry())
-                        .county(notificationDetails.getContactDetails().getCounty())
-                        .addressLine(notificationDetails.getContactDetails().getAddressLine())
-                        .email(notificationDetails.getContactDetails().getEmail())
-                        .build();
-                    refund.setContactDetails(newContact);
-                }
-
-                refund.setRefundStatus(RefundStatus.APPROVED);
-                refund.setUpdatedBy(SYSTEM_USER);
-                Refund refundUpdated = refundsRepository.findByReferenceOrThrow(reference);
-                refundUpdated.setStatusHistories(Arrays.asList(getStatusHistoryEntity(
-                    SYSTEM_USER,
-                    RefundStatus.APPROVED,
-                    LIBERATA_REJECT_UPDATE)
-                ));
-                String templateId =  refundsUtil.getTemplate(refund, statusUpdateRequest.getReason());
-                notificationService.updateNotification(headers, refund, null, templateId);
-
-            }
         }
         return new ResponseEntity<>("Refund status updated successfully", HttpStatus.NO_CONTENT);
     }
