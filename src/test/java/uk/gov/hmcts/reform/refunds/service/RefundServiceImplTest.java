43b11306681557490850c8a1ed35858cbc2e882d
Ollie-Smith <oliver.smith@hmcts.net>
2025-11-12 11:12:22 +0000

    PAY-8185 added backend for PayIt - checkstyle correction

--- src/test/java/uk/gov/hmcts/reform/refunds/service/RefundServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/refunds/service/RefundServiceImplTest.java
@@ -1397,8 +1397,9 @@ class RefundServiceImplTest {
             "payments-refund-AAA"
         )).sub("ZZ").build();
 
-        RefundRequest refundRequest = RefundRequest.refundRequestWith().paymentReference("1").reference("RF-1111-2222-3333").refundReason("RR001").ccdCaseNumber(
-            "2").refundAmount(BigDecimal.valueOf(666)).paymentAmount(BigDecimal.valueOf(666)).feeIds("3").contactDetails(
+        RefundRequest refundRequest = RefundRequest.refundRequestWith().paymentReference("1").reference("RF-1111-2222-3333")
+            .refundReason("RR001").ccdCaseNumber("2").refundAmount(BigDecimal.valueOf(666))
+            .paymentAmount(BigDecimal.valueOf(666)).feeIds("3").contactDetails(
             ContactDetails.contactDetailsWith().addressLine("ABC Street").email("mock@test.com").city("London").county(
                 "Greater London").country("UK").postalCode("E1 6AN").notificationType("Letter").build()).refundFees(
             Collections.singletonList(RefundFeeDto.refundFeeRequestWith().feeId(1).code("RR001").version("1.0").volume(1).refundAmount(
