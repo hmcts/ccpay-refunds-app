ab02e374dc96d5053bbbd4f58b671d445c1c6cb8
it.consulting@tutanota.com <it.consulting@tutanota.com>
2025-12-03 10:36:42 +0000

    WIP https://tools.hmcts.net/jira/browse/PAY-8237

--- src/main/java/uk/gov/hmcts/reform/refunds/services/RefundStatusServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/services/RefundStatusServiceImpl.java
@@ -15,11 +15,16 @@ import uk.gov.hmcts.reform.refunds.model.Refund;
 import uk.gov.hmcts.reform.refunds.model.RefundStatus;
 import uk.gov.hmcts.reform.refunds.model.StatusHistory;
 import uk.gov.hmcts.reform.refunds.repository.RefundsRepository;
+import uk.gov.hmcts.reform.refunds.repository.StatusHistoryRepository;
 import uk.gov.hmcts.reform.refunds.utils.RefundsUtil;
 import uk.gov.hmcts.reform.refunds.utils.StateUtil;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class RefundStatusServiceImpl extends StateUtil implements RefundStatusService {
@@ -34,8 +39,13 @@ public class RefundStatusServiceImpl extends StateUtil implements RefundStatusSe
     private static final String LIBERATA_REJECT_UPDATE = "Refund approved by system";
     private static final Logger LOG = LoggerFactory.getLogger(RefundStatusServiceImpl.class);
 
+    private static final Pattern REF_PATTERN = Pattern.compile("\\bRF-\\d{4}-\\d{4}-\\d{4}-\\d{4}\\b");
+
     @Autowired
     private RefundsRepository refundsRepository;
+
+    @Autowired
+    private StatusHistoryRepository statusHistoryRepository;
 
     @Autowired
     private NotificationService notificationService;
@@ -60,8 +70,9 @@ public class RefundStatusServiceImpl extends StateUtil implements RefundStatusSe
 
         Refund refund = refundsRepository.findByReferenceOrThrow(reference);
 
+
         if (statusUpdateRequest.getStatus().getCode().equals(ACCEPTED)) {
-            if (refund.getRefundStatus() == RefundStatus.APPROVED && refund.getUpdatedBy() == SYSTEM_USER) {
+            if (isAClonedRefund(refund)) {
                 //ACECEPTED for the second time from Liberata this is going down the PAYIT journey
                 refund.setRefundInstructionType(RefundsUtil.REFUND_WHEN_CONTACTED);
             }
@@ -72,11 +83,13 @@ public class RefundStatusServiceImpl extends StateUtil implements RefundStatusSe
                 LIBERATA_REASON)
             ));
 
+
             IdamTokenResponse idamTokenResponse = idamService.getSecurityTokens();
             String authorization =  "Bearer " + idamTokenResponse.getAccessToken();
             headers.put("authorization", Collections.singletonList(authorization));
 
-            Notification notificationDetails = notificationService.getNotificationDetails(headers, refund.getReference());
+
+            Notification notificationDetails = notificationService.getNotificationDetails(headers, getOriginalRefund(refund));
             if (notificationDetails == null) {
                 LOG.error("Notification not found. Not able to send notification.");
             } else {
@@ -129,5 +142,33 @@ public class RefundStatusServiceImpl extends StateUtil implements RefundStatusSe
             }
         }
         return new ResponseEntity<>("Refund status updated successfully", HttpStatus.NO_CONTENT);
+    }
+
+    private boolean isAClonedRefund(Refund refund) {
+        // We check if the refund status is APPROVED and if it was updated by the SYSTEM_USER. IF so,
+        // A cloned refund is one that was created as a result of a REISSUED status change.
+        return refund.getRefundStatus() == RefundStatus.APPROVED && refund.getUpdatedBy() == SYSTEM_USER;
+    }
+
+    private String getOriginalRefund(Refund refund) {
+        if (isAClonedRefund(refund)) {
+            // For cloned refunds, get the reference from the first REISSUED status history
+            List<StatusHistory> statusHistories = statusHistoryRepository.findByRefundOrderByDateCreatedDesc(refund);
+            Optional<StatusHistory> firstReissued = statusHistories
+                .stream()
+                .filter(history -> RefundStatus.REISSUED.getName().equals(history.getStatus()))
+                .findFirst();
+            return extractRefundReference(firstReissued.get().getNotes());
+        }
+        // For non-cloned refunds, return the current refund reference
+        return refund.getReference();
+    }
+
+    public String extractRefundReference(String input) {
+        Matcher matcher = REF_PATTERN.matcher(input);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        return null;
     }
 }
