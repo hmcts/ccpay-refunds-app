ee936c7799008b5cbec9462e0442df760ac54ae1
Antony Leons <antony@leons.dev>
2026-02-26 14:19:39 +0000

    CME-894: Add JwtIssuerValidator to enhance JWT validation in Spring Security

--- src/main/java/uk/gov/hmcts/reform/refunds/config/security/SpringSecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/refunds/config/security/SpringSecurityConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtIssuerValidator;
 import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -157,9 +158,11 @@ public class SpringSecurityConfiguration {
 
         OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(Arrays.asList(allowedAudiences));
         OAuth2TokenValidator<Jwt> withTimestamp = new JwtTimestampValidator();
+        OAuth2TokenValidator<Jwt> withIssuer = new JwtIssuerValidator(issuerUri);
 
         OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(
             withTimestamp,
+            withIssuer,
             audienceValidator
         );
         jwtDecoder.setJwtValidator(withAudience);
