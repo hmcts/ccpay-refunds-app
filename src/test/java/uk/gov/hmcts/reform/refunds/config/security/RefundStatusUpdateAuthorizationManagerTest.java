9d8ce0102213f7512b2f61335cc1b4a04fc198c8
Dave Jones <dave.jones@hmcts.net>
2026-08-12 20:11:48 +0100

    CME-896: Checkstyle error fix

--- src/test/java/uk/gov/hmcts/reform/refunds/config/security/RefundStatusUpdateAuthorizationManagerTest.java
+++ src/test/java/uk/gov/hmcts/reform/refunds/config/security/RefundStatusUpdateAuthorizationManagerTest.java
@@ -17,8 +17,8 @@ import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
 import java.util.List;
 import java.util.function.Supplier;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
